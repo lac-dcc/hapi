c8d6174ae97c107f1433bcd7518e8f8c0a9b6d39
Flávio Lúcio <flaviolcj@protonmail.com>
2020-10-03 21:08:54 -0300

    remove comments from YAML.kt

--- src/main/kotlin/tasks/YAML.kt
+++ src/main/kotlin/tasks/YAML.kt
@@ -1,45 +1,45 @@
-// package tasks;
+package tasks;
 
-// import java.io.File
+import java.io.File
 
-// import legalease.*
-// import utils.*
+import hapi.*
+import utils.*
 
-// class YAMLGenerator {
+class YAMLGenerator {
 
-//   fun generate(ir: IR, datamap: DataMap, filename: String) {
-//     val file = File(filename);
+  fun generate(ir: IR, datamap: DataMap, filename: String) {
+    val file = File(filename);
 
-//     file.bufferedWriter().use { out ->
-//       val resources = datamap["Resources"]!!;
-//       out.write("data: ${resources.atoms(resources.TOP)}\n");
+    file.bufferedWriter().use { out ->
+      val resources = datamap["Resources"]!!;
+      out.write("data: ${resources.atoms(resources.TOP)}\n");
 
-//       out.write("rules:\n");
+      out.write("rules:\n");
 
-//       (ir as NonTerminal).nonTerminal.forEach { (actor, actions) ->
-//         out.write("  - identities:\n");
-//         out.write("      users: ${actor}\n");
-//         (actions as NonTerminal).nonTerminal.forEach { (action, resources) ->
+      (ir as NonTerminal).nonTerminal.forEach { (actor, actions) ->
+        out.write("  - identities:\n");
+        out.write("      users: ${actor}\n");
+        (actions as NonTerminal).nonTerminal.forEach { (action, resources) ->
 
-//           out.write("      ${action}:\n");
-//           out.write("        data: ${resources as Terminal}\n");
-//         }
-//       }
-//     }
-//   }
-// }
+          out.write("      ${action}:\n");
+          out.write("        data: ${resources as Terminal}\n");
+        }
+      }
+    }
+  }
+}
 
-// fun main(args: Array<String>) {
+fun main(args: Array<String>) {
 
-//   if (args.size < 1){
-//     return usage();
-//   }
+  if (args.size < 1){
+    return usage();
+  }
 
-//   val priority = listOf("Actors", "Actions", "Resources")
-//   val ast = IR.generate(args[0], priority) as IRNode
+  val priority = listOf("Actors", "Actions", "Resources")
+  val ast = IR.generate(args[0], priority) as IRNode
   
-//   val outputFile = changeExtension(args[0], "yaml")
+  val outputFile = changeExtension(args[0], "yaml")
 
-//   val yamlGenerator = YAMLGenerator();
-//   yamlGenerator.generate(ast.ir, ast.dm, outputFile);
-// }
+  val yamlGenerator = YAMLGenerator();
+  yamlGenerator.generate(ast.ir, ast.dm, outputFile);
+}
