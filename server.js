203af4bb01b51caedfed1fa23650523681ba8da3
Vinicius Julião R <40807325+vrjuliao@users.noreply.github.com>
2020-12-07 03:25:08 -0300

    Update frontend (#2)
    * build(docker): Docker file created
    
    * build(script): Build script DONE
    
    * fix(gitignore): removign unecessary directories
    
    * fix(comments): Removing unecessary comments
    
    * chore(frontend): starting new layout
    
    * chore(frontend): spliting panel to receive typed code or files with code
    
    * chore(frontend): drag an drop files woring
    
    * chora(frontend): starting the right panel layout
    
    * chore(frontend): left side input file field
    
    * chore(frontend): input file for main.hp - DONE
    
    * chore(frontend):Main template - DONE
    
    * fix(responsiveness): making the layout responsive for ultrawide screens
    
    * backend(route): create the correct route to integrate backend with frontend
    
    * frontend(responsiveness): DONE
    
    * improve(UX): setting responsiveness features, backend messages
    
    * finally: can be released
    
    * New Release

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const { exec } = require('child_process');
 const fs = require('fs');
+const formidable = require('formidable');
+const { exception } = require('console');
 
-const outputDir = "static/output";
+const outputDir = "./static/output/";
 
 const hapiFile = `${outputDir}/main.hp`;
 
-// const program = "legalease/legalease.jar legalease/config.yaml";
-const program = `bin/hapi.jar ${hapiFile}`;
+const program = `bin/hapi.jar`;
 
 const resultHapiFile = `${outputDir}/main.yaml`;
 const resultActorsFile = `${outputDir}/actors.dot`;
@@ -21,49 +22,93 @@ const jsonParser = bodyParser.json()
 
 app.use(express.static('static'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/static/index.html'));
-});
-
-app.post('/generate', jsonParser, function (req, res) {
-
-  if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir);
-  }
-
-  const { hapi } = req.body;
-
-  fs.writeFileSync(hapiFile, hapi);
-
-  exec(`java -jar ${program}`,
-    (error, stdout, stderr) => {
-
-      if (error || stderr) {
-        console.log('stderr: ' + stderr);
-        res.json({ error: { msg: stderr, ...error } });
-        return;
-      }
-
-      const resultHapi = fs.readFileSync(resultHapiFile, "utf8");
-      const resultActors = fs.readFileSync(resultActorsFile, "utf8");
-      const resultResources = fs.readFileSync(resultResourcesFile, "utf8");
-      const resultActions = fs.readFileSync(resultActionsFile, "utf8");
-
-      res.json({
-        yaml: resultHapi,
-        actors: resultActors,
-        resources: resultResources,
-        actions: resultActions,
-        datamap: stdout
-      });
+function moveFiles(files){
+  var oldpath = '';
+  var newpath = '';
+  
+  for(key in files){
+    oldpath = files[key].path;
+    newpath = files[key].name;
+    fs.rename(oldpath, outputDir+newpath, function (err) {
+      if (err) throw err;
     });
+  }
+}
 
-});
+function removeOldFiles(dirPath) {
+  try { var files = fs.readdirSync(dirPath); }
+  catch(e) { return; }
+  if (files.length > 0)
+    for (var i = 0; i < files.length; i++) {
+      var filePath = dirPath + '/' + files[i];
+      if (fs.statSync(filePath).isFile())
+        fs.unlinkSync(filePath);
+    }
+};
 
-app.post('/verify', jsonParser, function (req, res) {
-  res.json({ api: 'verify' });
+function FileReceivingError(message){
+  this.message = 'Main file is required';
+  this.name = 'InexistentMain'
+}
+
+Object.size = function(obj) {
+  var size = 0, key;
+  for (key in obj) {
+      if (obj.hasOwnProperty(key)) size++;
+  }
+  return size;
+}; 
+
+app.post('/generate', function(req, res, next){
+
+  var fs = require('fs');
+  var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var mainFileName = undefined;
+      try{
+        if (!files.main)
+          throw new FileReceivingError('Main file is required');
+        if(Object.size(files) > 6)
+          throw new FileReceivingError('Files quantity overflow');
+          
+        mainFile = files.main.name.substr(0, files.main.name.lastIndexOf("."));
+        removeOldFiles(outputDir);
+        moveFiles(files);
+      } catch (e){
+        res.status(500);
+        res.json({ error: { msg: e.message}});
+      }
+      // mainFile = outputDir+files.main.name;
+      exec(`java -jar ${program} ${outputDir+mainFile}.hp`,
+        (error, stdout, stderr) => {
+        if (error || stderr) {
+          // console.log('stderr: ' + stderr);
+          res.status(500);
+          res.json({ error: { msg: stderr, ...error } });
+          return;
+        }
+
+        const resultHapi = fs.readFileSync(`${outputDir+mainFile}.yaml`, "utf8");
+        const resultActors = fs.readFileSync(resultActorsFile, "utf8");
+        const resultResources = fs.readFileSync(resultResourcesFile, "utf8");
+        const resultActions = fs.readFileSync(resultActionsFile, "utf8");
+        const resultMatrix = fs.readFileSync(`${outputDir+mainFile}.html`, "utf8");
+
+        res.json({
+          yaml: resultHapi,
+          actors: resultActors,
+          resources: resultResources,
+          actions: resultActions,
+          matrix: resultMatrix,
+          datamap: stdout
+        });
+      });    
+    });
 });
 
 app.listen(8080, () => {
   console.log("server running!");
+  if(!fs.existsSync(outputDir) ||
+    (fs.existsSync(outputDir) && !fs.statSync(outputDir).isDirectory()))
+    fs.mkdirSync(outputDir);
 });
--- static/index.js
+++ static/index.js
@@ -30,40 +30,320 @@ function genDot(svgId, dot) {
     });
 }
 
-$(document).ready(function () {
+function generate(){
 
-  $("textarea#yaml").val("");
+  // add d-flex class
+  $("div#load-overlay").addClass("d-flex");
+
+  const request = new XMLHttpRequest();
+  const formData = new FormData();
+  var mainCode = undefined;
+
+  if (document.getElementById('toggle-main-code').checked ) {
+    const typedCode = $('textarea#code').val();
+    mainCode = new File(typedCode.split('\n'), "main.hp", {
+      type: "text/plain",
+    });
+  } else {
+    mainCode = mainFile;
+    //ToDo: throws an error if mainFile is undefined
+  }
+
+  // append main file at formData
+  formData.append('action', 'hapicode');
+  formData.append('main', mainCode);
+
+  // append importFiles at formData
+  for(i=0; i< filesForUp.length; i++){
+    formData.append('import'+(i+1), filesForUp[i]);
+  }
+  request.onreadystatechange = receivingData;
+  request.open("POST", '/generate', true);
+  request.send(formData);
+}
+
+function receivingData() {
+  if (this.readyState == 4){
+    var response = JSON.parse(this.responseText);
+    if (this.status == 200)
+      receivingSuccess(response);
+    else
+      receivingError(response);
+
+    $("div#load-overlay").removeClass("d-flex");
+  }
+};
+
+function isHapiFile(path){
+  if(path.includes('.') &&
+    path.substr(path.lastIndexOf(".")+1) == 'hp'){
+      return true;
+  }
+  return false;
+}
+
+function receivingError(res){
+  var msg = "";
+  if(!res.error || !res.error.msg){
+    // unknown error
+    msg = "Unknown error";
+  } else {
+    msg = res.error.msg;
+  }
+  $('textarea#error-message').text(msg);
+  $('#errorModal').modal('show');
+}
+
+function receivingSuccess(res){
+  $('textarea#yaml-code').text(res.yaml);
+  $('#matrix').html(res.matrix);
+  genDot('actorsGraph', res.actors);
+  genDot('resourcesGraph', res.resources);
+  genDot('actionsGraph', res.actions);
+  $('input#preview').removeClass('disabled');
+  $('input#preview').removeAttr('disabled');
+  $('#exampleModalLong').modal('show');
+  
+}
+
+$(document).ready(function () {
+  $('#toggle-main-code')[0].checked = true;
 
   $("#generate").click(function (e) {
 
-    const hapi = $('textarea#hapi').val();
-
-    const data = {
-      hapi
-    };
-
-    post('generate', data).then(({ error, ...res }) => {
-      if (error) {
-        return alert(error);
-      }
-
-      $("textarea#yaml").val(res.yaml);
-      genDot("actorsGraph", res.actors);
-      genDot("resourcesGraph", res.resources);
-      genDot("actionsGraph", res.actions);
-
-      $("#matrix").load("output/main.html");
-
-    });
+    generate();
 
     e.preventDefault();
   });
 
-  $("#verify").click(function (e) {
-    alert("WIP");
-    e.preventDefault();
+  $(document).on('change', ':file', function() {
+    var input = $(this),
+        numFiles = input.get(0).files ? input.get(0).files.length : 1,
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [numFiles, label]);
+  });
+
+  $("#customFile").on('fileselect', function() {
+    var inputedFiles = document.getElementById('customFile').files;
+    addFiles(inputedFiles);
+  });
+
+  $("#customFile2").on('fileselect', function() {
+    var mainFile = document.getElementById('customFile2').files[0];
+    addMain(mainFile);
+  });
+
+  $('#toggle-main-code').on('change', function(ev) {
+    var textArea = document.getElementById('code');
+    var dropArea = document.getElementById('file-main');
+    var fileInputToggleLabel = document.getElementById('fileInput');
+    var typeCodeToggleLabel = document.getElementById('typeCode');
+    
+    if(ev.target.checked){
+      dropArea.setAttribute('disabled', '');
+      textArea.removeAttribute('disabled');
+      toggleMain(textArea, dropArea, typeCodeToggleLabel, fileInputToggleLabel);
+      
+    } else {
+      dropArea.removeAttribute('disabled');
+      textArea.setAttribute('disabled', '');
+      toggleMain(dropArea, textArea, fileInputToggleLabel, typeCodeToggleLabel);
+    }
+  });
+
+  $('.toggle-text').on('click', function(ev){
+    var toggleLabel = ev.target;
+    if(!toggleLabel.classList.contains('selected'))
+      document.getElementById('toggle-main-code').click();
   });
 });
 
+function toggleMain(toAble, toDisable, toggleTrue, toggleFalse){
+  toggleTrue.classList.add('selected');
+  toggleFalse.classList.remove('selected');
+  toAble.classList.remove('disabled');
+  toDisable.classList.add('disabled');
+}
 
+var mainFile = undefined;
+function addMain(_mainFile){
+  if(!isHapiFile(_mainFile.name)){
+    // TODO: popup with error is not a Hapi File
+    return;
+  }
+  filenameArea = document.getElementById('filename-row');
+  if (mainFile != undefined){
+    mainFile = _mainFile;
+    var span = filenameArea.children[0];
+    span.textContent = _mainFile.name;
+    return;
+  }
 
+  mainFile = _mainFile;
+
+  var span = document.createElement('span');
+  span.appendChild(document.createTextNode(_mainFile.name));
+  span.className = 'col-10 file-name';
+  filenameArea.appendChild(span);
+
+  var button = document.createElement('button');
+  button.addEventListener('click', function(ev){
+    removeMain(ev.target.parentElement);
+  });
+  button.className = 'btn btn-danger remove-button'
+  button.appendChild(document.createTextNode('X'));
+  button.setAttribute('type', 'button');
+  filenameArea.appendChild(button);
+}
+
+function removeMain(filenameArea){
+  mainFile = undefined
+  filenameArea.innerHTML = '';
+}
+
+function removeFile(removedLi){
+  console.log(removedLi)
+  // get index to remove
+  var index = parseInt(removedLi.getAttribute('index'));
+  
+  // remove the indexed element from the <ul>
+  var ul = removedLi.parentElement;
+  ul.removeChild(removedLi);
+
+  //apply FileForUp.splice(i, 1)
+  filesForUp.splice(index, 1);
+
+  //reset the attr (index, id) for each <li>
+  var children = ul.children;
+  for(i=0; i< children.length; i++){
+    children[i].setAttribute('index', i)
+  }
+  updateCounter();
+}
+
+var filesForUp = [];
+var maxFiles = 5;
+function addFiles(files){
+  var size = files.length;
+  if(size + filesForUp.length > maxFiles){
+    // put a message to show maxFiles overflow
+    return false;
+  }
+
+  var ul = document.getElementById('upload-list');
+
+  for(var i=0; i<size; i++){
+    if(!isHapiFile(files[i].name)){
+      // TODO: popup with error: Is not a Hapi file
+      return;
+    }
+  }
+
+  for(var i=0; i<size; i++){
+    var li = document.createElement('li');
+    li.className = 'list-group-item d-flex justify-content-between align-items-center'
+    
+    var span = document.createElement('span');
+    span.appendChild(document.createTextNode(files[i].name));
+    li.appendChild(span);
+    
+    var button = document.createElement('button');
+    button.addEventListener('click', function(ev){
+      removeFile(ev.target.parentElement);
+    });
+    button.className = 'btn btn-danger btn-sm'
+    button.appendChild(document.createTextNode('X'));
+    button.setAttribute('type', 'button');
+    li.appendChild(button);
+    
+    
+    var elemNumber = filesForUp.length+i;
+    li.setAttribute('index', elemNumber);
+    ul.appendChild(li);
+    filesForUp.push(files[i])
+  }
+  updateCounter();
+}
+
+function updateCounter(){
+  var counter = document.getElementById('counter');
+  counter.innerText = ''+filesForUp.length+'/'+maxFiles;
+}
+
++ function($) {
+  'use strict';
+
+  var dropZone = document.getElementById('drop-zone');
+
+  dropZone.ondrop = function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    this.className = 'col-11  upload-drop-zone';
+    $('#label-drop')[0].className = 'col-auto';
+    addFiles(e.dataTransfer.files)
+  }
+
+  dropZone.ondragover = preventDrag;
+
+  dropZone.ondragleave = function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    this.className = 'col-11 upload-drop-zone';
+    $('#label-drop')[0].className = 'col-auto';
+    return false;
+  }
+    
+  dropZone.ondragenter = preventDrag;
+
+  dropZone.ondragstart = preventDrag;
+}(jQuery);
+
+function preventDrag(e){
+  e.stopPropagation();
+  e.preventDefault();
+  e.dataTransfer.dropEffect = 'move';
+  this.className = 'col-11 upload-drop-zone drop';
+
+  $('#label-drop')[0].className = 'col-auto label-blue';
+  return false;
+}
+
++ function($) {
+  'use strict';
+
+  var dropMain = document.getElementById('drop-main');
+
+  dropMain.ondrop = function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    if(document.getElementById('toggle-main-code').checked)
+      return;
+    this.className = 'col-9';
+    addMain(e.dataTransfer.files[0])
+  }
+
+  dropMain.ondragover = preventDragMain;
+
+  dropMain.ondragleave = function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    if(document.getElementById('toggle-main-code').checked)
+      return;
+    this.className = 'col-9';
+    return false;
+  }
+    
+  dropMain.ondragenter = preventDragMain;
+
+  dropMain.ondragstart = preventDragMain;
+}(jQuery);
+
+function preventDragMain(e){
+  e.stopPropagation();
+  e.preventDefault();
+  if(document.getElementById('toggle-main-code').checked)
+    return;
+  e.dataTransfer.dropEffect = 'move';
+  this.className = 'col-9 drop';
+  return false;
+}
