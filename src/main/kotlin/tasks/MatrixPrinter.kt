eb23e78dd404b92f60760f29bad9453b5682af05
Yowgf <alexthomasmol@gmail.com>
2020-12-16 23:36:04 -0300

    Refactoring and doc
    Some unused imports were removed, some things renamed, and so on.

--- src/main/kotlin/tasks/MatrixPrinter.kt
+++ src/main/kotlin/tasks/MatrixPrinter.kt
@@ -1,10 +1,8 @@
-package tasks;
+package tasks
 
 import hapi.*
 import utils.*
 
-import java.io.FileInputStream
-import java.io.InputStream
 import java.io.OutputStreamWriter
 import java.io.FileOutputStream
 import java.io.File
@@ -19,9 +17,24 @@ import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.transform.stream.StreamResult
 import javax.xml.transform.dom.DOMSource
 
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 
+/**
+ * The class responsible for the matrix representation of a Hapi policy.
+ * <p>
+ *     It implements the Kotlin matrix representation, as well as routines to
+ *     generate its respective serialized formats, namely HTML and JSON.
+ * Usage example:
+ * <pre>
+ *     // generate matrix
+ *     val matrix = MatrixPrinter(actorsDataMap.elements(),
+ *                                resourcesDataMap.elements())
+ *     matrix.populateMatrix(irNode.ir)
+ *     // HTML and JSON
+ *     matrix.generateHtmlFile("Output.html")
+ *     matrix.generateJsonFile("Output.json")
+ * </pre>
+ */
 class MatrixPrinter {
   private val resources: Map<String, Int>
   private val indexedResources: Array<String>
@@ -29,11 +42,18 @@ class MatrixPrinter {
   private val indexedActors: Array<String>
   private val matrix: Array<Array<Map<String, Int>>>
 
+  /**
+   * Primary constructor.
+   * Constructs the internal objects indexedResources and indexedActors, and
+   * initializes matrix as a dummy object.
+   * @param actors    names of the actors
+   * @param resources names of the resources
+   */
   constructor(actors: Set<String>, resources: Set<String>) {
 
     // associate the resources names with the matrix indexes
     var index: Int = 0
-    this.resources = resources.associateWith({index++});
+    this.resources = resources.associateWith {index++}
     indexedResources = Array<String>(resources.size) {""}
     for ((key, value) in this.resources) {
       indexedResources[value] = key
@@ -41,7 +61,7 @@ class MatrixPrinter {
 
     // associate the actors names with the matrix indexes
     index = 0
-    this.actors = actors.associateWith({index++});
+    this.actors = actors.associateWith {index++}
     indexedActors = Array<String>(actors.size) {""}
     for ((key, value) in this.actors) {
       indexedActors[value] = key
@@ -55,6 +75,12 @@ class MatrixPrinter {
     }
   }
 
+  /**
+   * Fills the matrix with the ir actors, resources and their corresponding
+   * action status, for each action available.
+   * @param ir the policy intermediate representation
+   * @see   IR
+   */
   public fun populateMatrix(ir: IR) {
     //transform the IR data in a matrix
     for ((actor, actions) in (ir as NonTerminal).nonTerminal) {
@@ -67,6 +93,15 @@ class MatrixPrinter {
     }
   }
 
+  /**
+   * Generates and fills the target HTML serialized file with the properly
+   * formatted contents of the Hapi matrix.
+   * <p>
+   *     Makes use of the Kotlin DSL for HTML
+   *     <a href="https://github.com/Kotlin/kotlinx.html">kotlinx.html</a>
+   *     to easily create the java object.
+   * @param htmFileName target file
+   */
   public fun generateHtmlFile(htmFileName: String) {
     val document = DocumentBuilderFactory.newInstance().
       newDocumentBuilder().newDocument()
@@ -131,31 +166,52 @@ class MatrixPrinter {
     }
   }
 
-  public fun generateJSONFile(JSONFIleName: String) {
-    // Keys are actor, resource, action
-    val gsonMatrix: Map<Map<Map<Int>>> =
-      {
-        for (actor in indexedActors) {
-          for (resource in indexedResources) {
-
-          }
-        }
-        mapOf()
+  /**
+   * Generates and fills the target JSON serialized file with the properly
+   * formatted contents of the Hapi matrix.
+   * <p>
+   *     Makes use of the JSON serialization and deserialization library
+   *     <a href="https://github.com/google/gson">Gson</a>
+   *     to easily create the java object.
+   * @param jsonFileName the target file
+   */
+  public fun generateJsonFile(jsonFileName: String) {
+    /*
+      Aims to generate a three-layer deep map, so that GsonBuilder may be able
+        to build a JSON with the right names associated.
+      At a conceptual level, keys are actor, resource, action, respectively.
+    */
+    val gsonMap: Map<String, Map<String, Map<String, Int>>> =
+      mutableMapOf<String, Map<String, Map<String, Int>>>().apply {
+          putAll(indexedActors.toSet().associateWith { actor ->
+              mutableMapOf<String, Map<String, Int>>().apply {
+                  putAll(indexedResources.toSet().associateWith { resource ->
+                      matrix[actors[actor] as Int][resources[resource] as Int]
+                  })
+              }
+          })
       }
 
-
     val gsonPretty = GsonBuilder().setPrettyPrinting().create()
-    val jsonTutsArrayPretty: String = gsonPretty.toJson(matrix)
+    val jsonTutsArrayPretty: String = gsonPretty.toJson(gsonMap)
 
-    val jsonOutFile = FileWriter(JSONFIleName)
+    val jsonOutFile = FileWriter(jsonFileName)
     jsonOutFile.write(jsonTutsArrayPretty)
     jsonOutFile.close()
   }
 }
 
+/**
+ * Builds the matrix and generates both HTML and JSON output files.
+ * This is called by the gradle task "matrix".
+ *
+ * @param args the Hapi policy file name. The output serialized files shall have
+ *             the same root name (everything but the extension), including the
+ *             specified directory.
+ */
 fun main(args: Array<String>) {
   if (args.size < 1) {
-    return usage();
+    return usage()
   }
 
   val filepath = args[0]
@@ -176,6 +232,6 @@ fun main(args: Array<String>) {
   var matrixOutputFile = changeExtension(filepath, "html")
   matrix.generateHtmlFile(matrixOutputFile)
 
-  matrixOutputFile = changeExtension(filepath, "json");
-  matrix.generateJSONFile(matrixOutputFile);
+  matrixOutputFile = changeExtension(filepath, "json")
+  matrix.generateJsonFile(matrixOutputFile);
 }
