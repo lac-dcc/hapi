ea69688dd9a013692b0b7228cb36ef97214252ec
Yowgf <alexthomasmol@gmail.com>
2021-03-02 12:58:50 -0300

    Renamed Check3D class

--- src/main/kotlin/utils/DataMapOrderChecker.kt
+++ src/main/kotlin/utils/DataMapOrderChecker.kt
@@ -2,18 +2,18 @@ package utils
 
 import hapi.*
 
-class Check3DException(message: String) : Exception(message)
+class DataMapOrderError(message: String) : Exception(message)
 
 /* Checks that the datamap provided has exactly three keys, in the
  *   following order: ("Actors", "Actions", "Resources").
  */
-class Check3D(datamap: DataMap) {
+class DataMapOrderChecker(datamap: DataMap) {
   init {
     val dmKeys = datamap.keys.toList()
     val expectedDmKeys = listOf("Actors", "Actions", "Resources")
 
     if (dmKeys != expectedDmKeys) {
-      throw Check3DException("Expected data statements for ${expectedDmKeys.toString()}" +
+      throw DataMapOrderError("Expected data statements for ${expectedDmKeys.toString()}" +
         ", got ${dmKeys.toString()}")
     }
   }
